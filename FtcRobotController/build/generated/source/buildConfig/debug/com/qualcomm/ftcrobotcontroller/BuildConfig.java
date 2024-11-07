3f9ea489688cac27d0f9f3be5071ea3363d85fcc
antoniasmithmyer <smitha25@polyprep.org>
2024-11-07 16:41:44 -0500

    writs

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-11-06T17:51:16.723-0500";
+  public static final String APP_BUILD_TIME = "2024-11-07T16:34:04.682-0500";
 }
