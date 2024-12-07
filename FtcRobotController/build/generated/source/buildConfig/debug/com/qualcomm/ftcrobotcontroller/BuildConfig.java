129bc30666cd73d1baa126edbf150eaeba907711
Vencat <zhous25@polyprep.org>
2024-12-07 12:55:38 -0500

    i

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,9 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
+<<<<<<< Updated upstream
   public static final String APP_BUILD_TIME = "2024-12-07T12:21:37.147-0500";
+=======
+  public static final String APP_BUILD_TIME = "2024-12-07T12:17:09.020-0500";
+>>>>>>> Stashed changes
 }
